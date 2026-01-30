b6e14926e92756354161445835c2cf0fb2d56c10
naveenkumar80 <naveenkumar80032@gmail.com>
2026-01-30 11:33:10 +0530

    new pattern added

--- Patterns/Inverted_Triangle2.cpp
+++ Patterns/Inverted_Triangle2.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main()
 {
-    
+    int i,j,n;
+    cout<<"enter the number of lines :";
+    cin>>n;
+    for (i = 0 ; i < n ; i++){
+        for(j = 0; j < i; j++){
+            cout<<" ";
+        }
+        for(int k = n - i; k > 0; k--){
+            cout<<n - i;
+        }
+        cout<<endl;
+    }
     return 0;
 }
