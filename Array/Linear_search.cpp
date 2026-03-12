c2ced84893f03c60db775ebb96e44e41c3e30bd6
naveenkumar80 <naveenkumar80032@gmail.com>
2026-03-12 20:26:28 +0530

    linear search added

--- Array/Linear_search.cpp
+++ Array/Linear_search.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int linearSearch(int arr[], int size, int target){
+
+    for (int i = 0 ; i < size; i++){
+        if (arr[i] == target){
+            return i;
+        }
+    }
+    return -1;
+
+}
+
 int main()
 {
+    int arr[] = {22,44,56,34,63,99,78,8,4,6};
+    int size = sizeof(arr)/sizeof(int);
+    int target = 8;
     
-    return 0;
+    cout<<linearSearch(arr,size,target);
 }
