07fd2ce59ff8205cdad90eab6a84d59a662dbaae
naveenkumar80 <naveenkumar80032@gmail.com>
2026-03-14 12:03:34 +0530

    reverse an array

--- Array/Reverse a given array.cpp
+++ Array/Reverse a given array.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
 using namespace std;
 
+int reverseArray(int arr[], int size){
+    int start = 0 , end = size -1 ;
+    while(start<=end){
+        swap(arr[start],arr[end]);
+        start +=1;
+        end -=1;
+    }
+    for (int i = 0; i < size; i++) {
+    cout << arr[i] << " ";
+    }
+    return 0;
+}
+
 int main()
 {
-    
+    int arr[] ={3,4,57,5,23,52,99,43};
+    int size = sizeof(arr)/sizeof(int);
+    reverseArray(arr,size);
     return 0;
 }
