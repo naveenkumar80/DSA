46580bf09af6ad9d80a8d026bb99fc335cc6d8f3
naveenkumar80 <naveenkumar80032@gmail.com>
2026-04-04 20:22:20 +0530

    binary search added

---
+++ Array/Binary search.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {3,4,5,6,7,8,9,12,14,16};
+    int target = 12;
+    int length = 10;
+    int start = 0 , end = length - 1;
+
+    while(start <= end){
+        int mid = (start + end)/ 2 ;
+        if (target > arr[mid]){
+            start = mid + 1;
+        }
+        else if (target < arr[mid])
+        {
+            end = mid - 1;
+        }
+        else if(target == arr[mid]){
+            cout<< mid;
+            return 0;
+        }
+    }
+    
+    return 0;
+}
